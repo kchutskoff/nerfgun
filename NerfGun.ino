e0dd63617be553264235bf6a3425502f64712915
Kyle Chutskoff <kchutskoff@gmail.com>
2019-07-21 12:49:37 -0700

    Moved defines to constants and added docs about ESCs

--- NerfGun.ino
+++ NerfGun.ino
@@ -1,19 +1,24 @@
 #include <Servo.h>
 
-#define MOTOR_LEFT_PIN 9
-#define MOTOR_RIGHT_PIN 10
-#define TRIGGER_PIN 7
-#define SOLENOID_PIN 5
-#define MOTOR_DUTY_MIN 1000
-#define MOTOR_DUTY_MAX 2000
+// defining pins for various hardware devices
+const uint8_t MOTOR_LEFT_PIN = 9;
+const uint8_t MOTOR_RIGHT_PIN = 10;
+const uint8_t TRIGGER_PIN = 7;
+const uint8_t SOLENOID_PIN = 5;
 
-Servo MotorLeft;
-Servo MotorRight;
+// the minimum and maximum duty cycle that the ESCs for the motors understand.
+// Note, you need to make sure the ESC has been calibrated to use these duty cycles (see "Configuring ESCs" in README for more info)
+const int MOTOR_DUTY_MIN = 1000;
+const int MOTOR_DUTY_MAX = 2000;
 
 const float MotorLeftToRightRatio = 1; // control the ratio between motors, may have to change to something variable later
 const int SolenoidFireMS = 100; // how long the solenoid is fired to send a shot
 const int SolenoidResetMS = 150; // how long the solenoid waits after firing before it can fire again
 const int ButtonDebounceMS = 50; // how long to ignore inputs afer it changes
+
+
+Servo MotorLeft;
+Servo MotorRight;
 
 // Debounces a button by ignoring input for a small period of time after a change is detected, returns the debounced reading
 int CheckDebounceButton(uint8_t pin, int lastValue, int& lastChange, int debounceTimeout){
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nerfgun
-A brushless motor nerf gun arduino project 
+A Arduino project to build a brushless motor nerf gun.
+
+## Configuring ESCs
+Brushless motors use Electronic Speed Controllers (ESC) to control them. You can read into them more elsewhere, but essentially the Arduino talks to the ESCs with a 50 Mhz PWM signal (via the Servo library) to tell them what to do. The ESC interprets that signal and drives the motor accordingly.
+
+Generally the PWM signal varies between 900-1100 microsecond duty cycle for minimum speed and 1900-2000 microsecond duty cycle for max speed. Many ESCs can be configured to adjust this range. For this project, I've adjusted the range to be between 1000 and 2000 microseconds.
+
+Programming an ESC varies between makes and models, so consult the information provided by the manufacturer of your motor. The motors and ESCs I bought were unbranded and unlabeled, but I found some documentation that appears to match its functionality. You can find it in [External Docs](External%20Docs/30A_BLDC_ESC_Product_Manual.pdf).
+
+For this project, I have also enabled the break for the motors, so they will spin down quickly instead of free-wheeling when the speed is reduced.
